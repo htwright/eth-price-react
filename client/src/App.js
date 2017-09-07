0bda0e4e188692475c0bb58a125fff3f5e6424d9
htwright <harrisontwright@gmail.com>
2017-09-06 22:54:19 -0500

    retrieving my current wallet balance, local rpc env working

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      balance : 0
+    };
+  }
+
+  fetchBalance(){
+    return fetch('/api/hello')
+    .then(data => {
+      // console.log(data.json());
+      return data.json();
+    }).then(data =>{
+      return this.setState({balance:data});
+    }).catch(err => console.error(err));
+  }
+  componentDidMount(){
+    return this.fetchBalance();
+  }
+
   render() {
+    // let balance = this.fetchBalance();
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +34,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <span> CURRENT ETH BALANCE FOR ME = {this.state.balance}</span>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-
+const Web3 = require('web3');
 const app = express();
 
 // API endpoints go here!
@@ -8,6 +8,19 @@ const app = express();
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/api/hello', (req, res) => {
+  let web3;
+  if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  }
+  console.log(web3.eth.getBalance('0x9aF830259c46900A84b9F7E99fB7DA4FCe034bA3'))
+  res.status(200).json(web3.eth.getBalance('0x9aF830259c46900A84b9F7E99fB7DA4FCe034bA3'));
+});
+
 
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
