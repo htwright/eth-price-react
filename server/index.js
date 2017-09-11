2dc803b9303ff6b309c8a7bbc7475207486d9333
htwright <harrisontwright@gmail.com>
2017-09-11 16:26:35 -0500

    cleaned up

--- server/index.js
+++ server/index.js
@@ -3,26 +3,20 @@ const express = require('express');
 const Web3 = require('web3');
 const app = express();
 
-// API endpoints go here!
 let web3 = undefined;
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
-  // set the provider you want from Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
 app.get('/api/hello', (req, res) => {
-  // web3.eth.getBlock('latest')
-  // .then(console.log);
-  
   let test = web3.eth.getBalance('0x72711c31A72E61D6cDf17A7E881cCc5561c228A7')
   console.log(test.toString(10))
   res.status(200).json(web3.eth.getBalance('0x72711c31A72E61D6cDf17A7E881cCc5561c228A7'))
 });
 
 app.get('/api/balance/:wallet', (req, res) => {
-// web3.eth.getBalance(req.params.wallet).then(data => res.status(200).json(data)).catch(err => console.error(err));
 let balance = web3.eth.getBalance(req.params.wallet)
 res.json(web3.fromWei(balance));
 });
@@ -34,18 +28,28 @@ app.get('/api/transactions', (req, res) => {
 });
 
 app.get('/api/syncing', (req, res) => {
-  console.log(web3.eth.isSyncing((error, result) => {if(!error){console.log(result);}else{console.error(error);}}))
-  
-  
+  let obj = web3.eth.isSyncing((error, result) => {
+    if(!error){
+      return result;
+    }else{
+      console.error(error);
+    }
+  });
+  if(obj){
+    res.json(obj.lastSyncState);
+  }
 });
 
-// Serve the built client
+app.get('/api/network', (req, res) => {
+  if(web3.version.network !== '1'){
+    res.json('connected to testnet');
+  } else {
+    res.json('connected to main net');
+  }
+});
+
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-
-
-// Unhandled requests which aren't for the API should serve index.html so
-// client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
     const index = path.resolve(__dirname, '../client/build', 'index.html');
     res.sendFile(index);
